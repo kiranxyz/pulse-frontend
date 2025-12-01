6a37f19d4b1332e19f3b5329fc4913ecd961d71a
kiranxyz <anila.saifan@gmail.com>
2025-12-01 17:06:43 +0100

    updated feature branch

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_PULSE_BACKEND_API_URL,
   fetchOptions: {
     credentials: "include",
   },
